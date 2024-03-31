4590527cb9ac4f5bd65402dc9fd17ac822561c4f
TyshchenkoYulia <145122847+TyshchenkoYulia@users.noreply.github.com>
2024-03-31 15:30:05 +0300

    finish task 2

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,12 +1,13 @@
 import css from "./FriendListItem.module.css";
-// import clsx from 'clsx';
+import clsx from 'clsx';
 
-export default function FriendListItem({avatar, name, isOnline}) {
+export default function FriendListItem({ friend: { avatar, name, isOnline } }) {
     return (
         <div>
             <img className={css.avatar} src={avatar} alt="Avatar" width="48" />
             <p className={css.name}>{name}</p>
-            <p className={css.isOnline}>{isOnline}</p>
+            <p className={clsx(css.text, isOnline ? css.isOnline : css.isOffline)}>
+                {isOnline ? 'Online' : 'Offline'}</p>
         </div>
     )
 }
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -14,12 +14,16 @@ export default function TransactionHistory({items}) {
 
             <tbody>
                 {items.map((item) => {
-                    <tr>
-                        <td>{item.type}</td>
-                        <td>{item.amount}</td>
-                        <td>{item.currency}</td>
-                    </tr>
-                })}
+                    return (
+                        <tr key={item.id}>
+                            <td>{item.type}</td>
+                            <td>{item.amount}</td>
+                            <td>{item.currency}</td>
+                        </tr>
+                    );
+                }
+                    
+                )}
                 
             </tbody>
         </table>
